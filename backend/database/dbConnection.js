e3c7fce8462f239f8b7eeea604e43c943e8784f6
kartiksharma0047 <kartiksharma55109@gmail.com>
2024-12-08 03:08:04 +0530

    DataBase Connectivity

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection=()=>{
-    mongoose.connect(process.env.MONGO_URI,{
+    mongoose.connect(`${process.env.MONGO_URI}`,{
         dbName:"Hospital_Management_System_MERN"
     }).then(()=>{
         console.log("Connected to database!")
